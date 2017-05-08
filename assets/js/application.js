e423f0b4e0828ec5f6651871ba72e6bc9c8fc5e7
Brunno Pleffken <brunno.pleffken@outlook.com>
2017-05-08 12:36:26 -0300

    Add Particles.js to the header (and few other changes)

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,3 +1,12 @@
-// particlesJS.load('particles', '/assets/particles.json', function() {
-//   console.log('callback - particles.js config loaded');
-// });
+/**
+ * ADDICTIVEHUB.IO
+ * Website - https://addictivehub.io
+ * GitHub - https://github.com/addictivehub
+ */
+
+
+// Add particles
+
+particlesJS.load('particles', '/assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
